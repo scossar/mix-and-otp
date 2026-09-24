2b81a37a952445e5f28ade991e71aa97d5be5af6
Simon Cossar <scossar3@gmail.com>
2026-09-24 00:08:49 -0700

    add echo server

--- lib/kv.ex
+++ lib/kv.ex
@@ -8,7 +8,9 @@ defmodule KV do
       # A child specification; typically represented as `{module, options}`, i.e.
       # `{Registry, name: KV, keys: :unique}`
       {Registry, name: KV, keys: :unique},
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      {Task.Supervisor, name: KV.ServerSupervisor},
+      Supervisor.child_spec({Task, fn -> KV.Server.accept(4040) end}, restart: :permanent)
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
