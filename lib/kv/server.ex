d32da57a9979918af2eb095ea7ffec62503c8c2c
Simon Cossar <scossar3@gmail.com>
2026-09-24 19:30:24 -0700

    Add KV.Command; improve KV.Server

--- lib/kv/server.ex
+++ lib/kv/server.ex
@@ -24,19 +24,36 @@ defmodule KV.Server do
   end
 
   defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
+    msg =
+      with {:ok, data} <- read_line(socket),
+           {:ok, command} <- KV.Command.parse(data),
+           do: KV.Command.run(command, socket)
 
+    write_line(socket, msg)
     serve(socket)
   end
 
   defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-    data
+    :gen_tcp.recv(socket, 0)
   end
 
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
+  defp write_line(_socket, :ok) do
+    :ok
+  end
+
+  defp write_line(socket, {:error, :unknown_command}) do
+    # Known error; write to the client
+    :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
+  end
+
+  defp write_line(_socket, {:error, :closed}) do
+    # The connection was closed, exit politely
+    exit(:shutdown)
+  end
+
+  defp write_line(socket, {:error, error}) do
+    # Unknown error; write to the client and exit
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
   end
 end
